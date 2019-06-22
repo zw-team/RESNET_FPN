2d1dcdb4a32d7121200fa8696b7855b7e894cadc
YezhenWang <309426989@qq.com>
2019-06-22 16:41:38 +0800

    2019 6.22

--- utils.py
+++ utils.py
@@ -7,18 +7,6 @@ import cv2
 import numpy as np
 import scipy
 
-
-class GroundTruthProcess(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size):
-        super(GroundTruthProcess, self).__init__()
-        self.kernel_size = kernel_size
-        self.kernel = torch.FloatTensor(torch.ones(out_channels, in_channels, kernel_size, kernel_size)).cuda()
-
-    def forward(self, x):
-        result = F.conv2d(x, self.kernel, bias=None, stride=self.kernel_size, padding=0)
-        return result
-
-
 def show(origin_map, gt_map, predict, index):
     figure, (origin, gt, pred) = plt.subplots(1, 3, figsize=(20, 4))
     origin.imshow(origin_map)
